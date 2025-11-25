c1a252c26393d894a19cbf692468f463b30eecd2
David Hempston <cyber_veio@hotmail.com>
2025-11-25 14:22:36 +0000

    Basic setup

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,10 +1,9 @@
 apt-get update
 apt-get -y install ca-certificates gnupg
 # We add the OpenVPN repo to get the latest version.
-if [[ $VERSION_ID == "16.04" ]]; then
-	echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" >/etc/apt/sources.list.d/openvpn.list
-	wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
-	apt-get update
-fi
+echo "deb http://build.openvpn.net/debian/openvpn/stable noble main" >/etc/apt/sources.list.d/openvpn.list
+wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
+apt-get update
+i
 # Ubuntu > 16.04 and Debian > 8 have OpenVPN >= 2.4 without the need of a third party repository.
 apt-get install -y openvpn iptables openssl wget ca-certificates curl
